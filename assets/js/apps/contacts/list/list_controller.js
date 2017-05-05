4b1f3423813a91bfd92e484a36ac5800af7595bd
TristanLobaugh <ttrader_atl@yahoo.com>
2017-05-05 09:53:03 -0400

    events and event bubbling

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -7,6 +7,15 @@ ContactManager.module('ContactsApp.List', (List, ContactManager, Backbone, Mario
 				collection: contacts
 			});
 
+			contactsListView.on('childview:contact:delete', (childview, model) => {
+				contacts.remove(model);
+			});
+
+			// Exercise
+			// contactsListView.on('childview:contact:warning', (childview, model) => {
+			// 	console.log('Highlighting toggled on model: ', model);
+			// });
+
 			ContactManager.regions.main.show(contactsListView);
 		}
 	};
--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,7 +1,32 @@
 ContactManager.module('ContactsApp.List', (List, ContactManager, Backbone, Marionette, $, _) => {
 	List.Contact = Marionette.ItemView.extend({
 		tagName: 'tr',
-		template: '#contact-list-item'
+		template: '#contact-list-item',
+		events: {
+			'click': 'highlightName',
+			// Exercise
+			// 'click td': 'alertCellText'
+			'click button.js-delete': 'deleteClicked'
+		},
+		highlightName(e) {
+			this.$el.toggleClass('warning');
+			// Exercise
+			// this.trigger('contact:warning', this.model);
+		},
+		// Exercise
+		// alertCellText(e) {
+		// 	alert($(e.target).text());
+		// },
+		deleteClicked(e) {
+			e.stopPropagation();
+			this.trigger('contact:delete', this.model);
+		},
+		remove() {
+			const self = this;
+			this.$el.fadeOut(() => {
+				Marionette.ItemView.prototype.remove.call(self);
+			});
+		}
 	});
 
 	List.Contacts = Marionette.CompositeView.extend({
